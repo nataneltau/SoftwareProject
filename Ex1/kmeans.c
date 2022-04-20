ce5f1baeb4e2128e9246065de8ba68853d07acc7
natanel <natanel@mail.tau.ac.il>
2022-04-20 15:32:18 +0300

    almost finished writing kmeans.c, remain: finish kmeans func and writing update_centroids func. then check if everything work

--- Ex1/kmeans.c
+++ Ex1/kmeans.c
@@ -19,11 +19,63 @@ struct linked_list{
 //that way i will able to allocate arrays and not work with lists, can use it in
 //while(line 32) and in array line 51
 
-double calc_dist(char *x, char *centroid, int dimension);
+//this function make an array of string to an array of float, it basically makes the vektor from string representation to double one
+double *makeDataDuoble(char *vektor_char, int dimension){//when using this func, the caller need to free memory at the end of the use
+    char *token = strtok(vektor_char, ",");
+    double *vektor_double = malloc(dimension);
+    int k = 0;
+
+    if(!vektor_double){//need to deal with it
+        return NULL;
+    }
+    
+    while(token != NULL){//convert from array of strings to array of doubles
+        vektor_double[k] = atof(token);
+        k++;
+        token = strtok(NULL, ",");
+
+    }//end of while
+
+    return vektor_double;
+
+}//end of function makeDataDuoble
+
+double calc_dist(char *x, char *centroid, int dimension);//this function calculate (x-centroid)^2
+
+int calc_argmin(int k, char *x, char (*arr_centroids)[k], int dimension);
+
+void update_centroids(char *cluster, int dimension, char *str_centroid_buffer);
 
 int calcLineLen(char *input_file);
 
 int numbersOfLines(char *input_file, int line_len);
+
+//let's build some functions
+int euclidean_norm( int k, char (*arr_centroids)[k], char (*arr_prev_centroids)[k], int dimension, int iter_num){
+
+    double small_than_e = 0.001;
+    double tmp;
+
+    if(iter_num == 0){//we are at the first iteration, return false
+        return 0;
+    }
+
+
+    for(int i = 0; i<k; i++){
+
+        tmp = calc_dist(arr_prev_centroids[i], arr_centroids[i], dimension);
+        tmp = fabs(tmp);
+        tmp = pow(tmp, 0.5);
+
+        if(tmp >= small_than_e){
+            return 0;
+        }//end of if
+
+
+    }//end of for
+    
+    return 1;
+}//end of function euclidean_norm
 
 int kmeans(int k, int max_iter, char *input_file, char *output_file){
 
@@ -40,7 +92,7 @@ int kmeans(int k, int max_iter, char *input_file, char *output_file){
 
     while(fscanf(fr, "%s", buff) == 1 ){
 
-        strncpy(data_points[i], buff, line_len+1);
+        strcpy(data_points[i], buff);//maybe use strncpy ?
         i++;
 
     }//end of while
@@ -61,22 +113,59 @@ int kmeans(int k, int max_iter, char *input_file, char *output_file){
     char arr_centroids[k][line_len];
     char arr_prev_centroids[k][line_len];
     char arr_clusters[k][data_num][line_len];
+    int index_for_cluster_k[k];
 
+    for(i = 0; i<k; i++){
+
+        index_for_cluster_k[i] = 0;
+
+    }//end of for
+    
     for(i=0; i<k; i++){
 
-        strncpy(arr_centroids[i], data_points[i], line_len+1);
+        strcpy(arr_centroids[i], data_points[i]);//maybe use strncpy ?
 
-    }//end of while
-
-
+    }//end of for
 
 
-    while((iter_num != max_iter)/* && !(euclidean_norm())*/){
-        //remain to complete in this func (kmeans) this while loop
+    int index_to_cluster;
+
+    while((iter_num != max_iter) && !(euclidean_norm(k, arr_centroids, arr_prev_centroids, dimension, iter_num))){
+        
+        
+        
+        for(int m = 0; m<data_num; m++){
+
+            memset(arr_clusters[m], 0, sizeof arr_clusters[m]);
+
+        }//end of for
+
+        for(int l = 0; l<k; l++){
+
+            strcpy(arr_prev_centroids[l], arr_centroids[l]);//maybe use strncpy ?
+
+        }//end of for
+        
+        for(int i=0; i<data_num; i++){
+
+            index_to_cluster = calc_argmin(k, data_points[i], arr_centroids, dimension);
+
+            strcpy(arr_clusters[index_to_cluster][index_for_cluster_k[index_to_cluster]], data_points[i]);
+
+            index_for_cluster_k[index_to_cluster]++;
+
+        }//end of for
+        //after the for, in arr_cluster[i] there is all the x_j that u_i is their closest centroid
+
+        //for j in range(0, k):
+         //   lst_centroids[j] = update_centroids(lst_clusters[j], dimension)
+        
+
+
+        //remain to complete in this func (kmeans) this while loop and writing update_centroids func
         //and the writing to file after it
 
 
-        //https://www.delftstack.com/howto/c/clear-array-in-c/ for lst_clusters = [[]] line 42 in python code
 
         iter_num++;
     }//end of while
@@ -118,31 +207,36 @@ int numbersOfLines(char *input_file, int line_len){
 
 }//end of function numbersOfLines
 
+int calc_argmin(int k, char *x, char (*arr_centroids)[k], int dimension){
+
+    double min_centr = calc_dist(x, arr_centroids[0], dimension);
+    int index_cluster = 0;
+    double tmp = min_centr + 1;
+
+    for(int i=1; i<k; i++){
+
+        tmp = calc_dist(x, arr_centroids[i], dimension);
+
+        if(tmp < min_centr){
+            min_centr = tmp;
+            index_cluster = i;
+        }//end of if
+
+    }//end of for
+
+    return index_cluster;
+
+}//end of function calc_argmin
+
 double calc_dist(char *x, char *centroid, int dimension){
 
-    char *token = strtok(x, ",");
-    double x_double[dimension];
-    int k = 0;
+    double *x_double;
+    double *centroid_double;
     double tmp = 0.0;
     double sum = 0.0;
     
-    while(token != NULL){//convert from array of strings to array of doubles
-        x_double[k] = atof(token);
-        k++;
-        token = strtok(NULL, ",");
-
-    }//end of while
-
-    token = strtok(centroid, ",");
-    double centroid_double[dimension];
-    k = 0;
-
-    while(token != NULL){//convert from array of strings to array of doubles
-        centroid_double[k] = atof(token);
-        k++;
-        token = strtok(NULL, ",");
-
-    }//end of while
+    x_double = makeDataDuoble(x, dimension);
+    centroid_double = makeDataDuoble(centroid, dimension);
 
     for(int i = 0; i<dimension; i++){
 
@@ -152,9 +246,17 @@ double calc_dist(char *x, char *centroid, int dimension){
 
     }//end of for
     
+    free(x_double);
+    free(centroid_double);
+
     return sum;
 
 }//end of function calc_dist
+
+void update_centroids(char *cluster, int dimension, char *str_centroid_buffer){
+
+   
+}//end of function update_centroids
 
 int main(int argc, char *argv[]){
 
@@ -175,4 +277,5 @@ int main(int argc, char *argv[]){
     //printf("%d", numbersOfLines("test line len.txt", 100));
    
     return 0;
+
 }//end of main func
