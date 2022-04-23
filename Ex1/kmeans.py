836afe57f6951c3a075d5aae01421b1977234334
natanel <natanel@mail.tau.ac.il>
2022-04-23 15:57:41 +0300

    added error handling to kmeans.py

--- Ex1/kmeans.py
+++ Ex1/kmeans.py
@@ -12,68 +12,98 @@ def kmean(k: int, max_iter = 200, input_file = "", output_file = ""):
 
 
     iter_num = 0
-    fr = open(input_file, "r")
-    inp_txt = fr.read()
-    data_points = inp_txt.split("\n")
-    data_num = len(data_points)
-    tmp = data_points[0].split(",")
-    dimension = len(tmp)
+    try:
+        fr = open(input_file, "r")
+    except OSError:
+        print("Invalid Input!")
+        return    
+
+    try:
+        inp_txt = fr.read()
+        data_points = inp_txt.split("\n")
+        data_num = len(data_points)
+        tmp = data_points[0].split(",")
+        dimension = len(tmp)
+    except:
+        print("An Error Has Occurred")
+        return
+
+    if(dimension <= 0 or data_num <=0):
+        print("Invalid Input!")
+        fr.close()
+        return
+    
+
 
     fr.close()
 
-    fw = open(output_file, "w")
+    try:
+        fw = open(output_file, "w")
+    except OSError:
+        print("Invalid Input!")
+        return
 
-    lst_centroids = []
-    lst_prev_centroids =[]
-    lst_clusters = [[]]
-    
-    for i in range(0, k):
-        lst_centroids.append(data_points[i])
-    
-
-    while True:
-        if iter_num == max_iter:
-            break
-        
-        if euclidean_norm(lst_centroids, lst_prev_centroids, dimension):
-            break
-        
-
+    try:
+        lst_centroids = []
+        lst_prev_centroids =[]
         lst_clusters = [[]]
-        for l in range(0, k-1):
-            lst_clusters.append([])
-
-        lst_prev_centroids = lst_centroids.copy()
-
-        for i in range(0, data_num-1):
-            lst_clusters[calc_argmin(data_points[i], lst_centroids, dimension, k)].append(data_points[i])
-        #after the for, in lst_cluster[i] there is all the x_j that u_i is their closest centroid
-
-        for j in range(0, k):
-            lst_centroids[j] = update_centroids(lst_clusters[j], dimension)
         
-        iter_num += 1
-    
-    #print(iter_num)
-    for element in lst_centroids:
-        fw.write(element + "\n")
-    
-    fw.close()
+        for i in range(0, k):
+            lst_centroids.append(data_points[i])
+        
+
+        while True:
+            if iter_num == max_iter:
+                break
+            
+            if euclidean_norm(lst_centroids, lst_prev_centroids, dimension):
+                break
+            
+
+            lst_clusters = [[]]
+            for l in range(0, k-1):
+                lst_clusters.append([])
+
+            lst_prev_centroids = lst_centroids.copy()
+
+            for i in range(0, data_num-1):
+                lst_clusters[calc_argmin(data_points[i], lst_centroids, dimension, k)].append(data_points[i])
+            #after the for, in lst_cluster[i] there is all the x_j that u_i is their closest centroid
+
+            for j in range(0, k):
+                lst_centroids[j] = update_centroids(lst_clusters[j], dimension)
+            
+            iter_num += 1
+        
+        #print(iter_num)
+        for element in lst_centroids:
+            fw.write(element + "\n")
+        
+        
+    except:
+        print("An Error Has Occurred")
+        
+    finally:
+        fw.close()
+            
 
 
 
 
 def euclidean_norm(lst_centroids, lst_prev_centroids, dimension):
-    small_than_e = 0.001
+    try:
+        small_than_e = 0.001
 
-    if len(lst_prev_centroids) == 0:
-        return False
-
-    for i in range(0, len(lst_centroids) ):
-        if (abs(calc_dist(lst_prev_centroids[i], lst_centroids[i], dimension)) ** (1/2)) >= small_than_e:
+        if len(lst_prev_centroids) == 0:
             return False
-    
-    return True
+
+        for i in range(0, len(lst_centroids) ):
+            if (abs(calc_dist(lst_prev_centroids[i], lst_centroids[i], dimension)) ** (1/2)) >= small_than_e:
+                return False
+        
+        return True
+    except:
+        raise Exception()
 
 
 #in the end I am not using thin function, but maybe will so not delete
@@ -94,69 +124,81 @@ def euclidean_norm(lst_centroids, lst_prev_centroids, dimension):
 #this function return the index of the closest cluster to x
 def calc_argmin(x, lst_centroids, dimension, k):
     
-    min_centr = calc_dist(x, lst_centroids[0], dimension)
-    index_cluster = 0
-    tmp = min_centr + 1
+    try:
+        min_centr = calc_dist(x, lst_centroids[0], dimension)
+        index_cluster = 0
+        tmp = min_centr + 1
 
-    for i in range (1, k):
+        for i in range (1, k):
 
-        tmp = calc_dist(x, lst_centroids[i], dimension)
+            tmp = calc_dist(x, lst_centroids[i], dimension)
 
-        if tmp < min_centr:
-            min_centr = tmp
-            index_cluster = i
-    
+            if tmp < min_centr:
+                min_centr = tmp
+                index_cluster = i
 
-    return index_cluster
+        return index_cluster
+        
+
+    except:
+        raise Exception()
 
 
 #this function return the distance between 2 point squared
 def calc_dist(x: str, centroid: str, dimension):
-    x = x.split(",")
-    #print(x)
-    x = [float(i) for i in x]
-    centroid = centroid.split(",")
-    centroid = [float(i) for i in centroid]
-    sum = 0.0
-    tmp = 0
+    try:
+        x = x.split(",")
+        #print(x)
+        x = [float(i) for i in x]
+        centroid = centroid.split(",")
+        centroid = [float(i) for i in centroid]
+        sum = 0.0
+        tmp = 0
 
-    for i in range (0, dimension ):
-        tmp = (x[i]-centroid[i]) ** 2
-        sum += tmp
-    
-    return sum
+        for i in range (0, dimension ):
+            tmp = (x[i]-centroid[i]) ** 2
+            sum += tmp
+        
+        return sum
+
+    except:
+        raise Exception()
 
 
 def update_centroids(cluster, dimension): #cluster is list
-    clus_len = len(cluster)
-    new_centroid = [0] * dimension
-    str_centroid = ""
+    try:
+        clus_len = len(cluster)
+        new_centroid = [0] * dimension
+        str_centroid = ""
 
-    if clus_len == 0:
-        return None
-    
-    for i in range (0, clus_len):
+        if clus_len == 0:
+            return None
         
-        tmp_lst = cluster[i].split(",")
+        for i in range (0, clus_len):
+            
+            tmp_lst = cluster[i].split(",")
 
-        for j in range(0, dimension ):
+            for j in range(0, dimension ):
 
-            new_centroid[j] += float(tmp_lst[j])
-
-
-    for m in range(0, dimension ):
-        new_centroid[m] = new_centroid[m] / clus_len
+                new_centroid[j] += float(tmp_lst[j])
 
 
-    for k in range (0, dimension - 1):
+        for m in range(0, dimension ):
+            new_centroid[m] = new_centroid[m] / clus_len
 
-        str_centroid += str('%.4f'%new_centroid[k]) + ","
-    
-    str_centroid += str('%.4f'%new_centroid[dimension - 1])
 
-    
-    
-    return str_centroid
+        for k in range (0, dimension - 1):
+
+            str_centroid += str('%.4f'%new_centroid[k]) + ","
+        
+        str_centroid += str('%.4f'%new_centroid[dimension - 1])
+
+        
+        
+        return str_centroid
+
+    except:
+        raise Exception()
 
 #print(len(sys.argv))
 if len(sys.argv) == 5:
